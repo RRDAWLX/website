a2486b9d2b639aeeccd4be021e33df8bff5e9eaf
longxiang.wang <longxiang.wang@vipshop.com>
2018-01-24 12:08:41 +0800

    所有get请求自动加时间戳url参数

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,30 +1,31 @@
-import axios from 'axios';
+import axios from 'axios'
+
+axios.interceptors.request.use(config => {
+  // console.log('request interceptor 1 invoked')
+  config.params = Object.assign({}, config.params, {t: Date.now()})
+  return config
+}, error => {
+  // console.log('request interceptor 2 invoked')
+  return Promise.reject(error)
+})
 
 axios.interceptors.response.use(res => {
-  if (!res.data.status || res.data.status !== 1) {
-    console.log('response interceptor 1 invoked');
-  }
-  let json = {
-    status: 0,
-    data: {}
-  };
+  /*if (!res.data.status || res.data.status !== 1) {
+    console.log('response interceptor 1 invoked')
+  }*/
 
   if (res && res.data) {
-    res.data.origin = res;
-    return res.data;
+    res.data.origin = res
+    return res.data
   } else {
     return {
       status: 0,
       origin: res
-    };
+    }
   }
 }, error => {
-  console.log('response interceptor 2 invoked');
+  // console.log('response interceptor 2 invoked')
+  return Promise.reject(error)
+})
 
-  return {
-    status: 0,
-    error
-  };
-});
-
-export default axios;
+export default axios
