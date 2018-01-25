b668a69f9a36e8e772799d6cca89575a76c16606
longxiang.wang <longxiang.wang@vipshop.com>
2018-01-25 14:35:56 +0800

    引入material-ui重写TopBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, {Component} from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import {connect} from 'react-redux'
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import TopBar from './components/TopBar/index'
 import Home from './views/Home/index'
@@ -10,19 +11,23 @@ import 'app.less'
 
 export default class App extends Component {
   render() {
-    return (<BrowserRouter>
-      <div className='router'>
-        <TopBar/>
-        <div className='routes'>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/test' component={Test}/>
-          <Route exact path='/edit/:articleId' component={Edit}/>
-        </div>
-      </div>
-    </BrowserRouter>)
+    return (
+      <MuiThemeProvider>
+        <BrowserRouter>
+          <div className='router'>
+            <TopBar/>
+            <div className='routes'>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/test' component={Test}/>
+              <Route exact path='/edit/:articleId' component={Edit}/>
+            </div>
+          </div>
+        </BrowserRouter>
+      </MuiThemeProvider>
+    )
   }
 }
 
 function selectState(state) {
-  return {user: state.user}
+  return { user: state.user }
 }
--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -3,6 +3,8 @@ import { NavLink, Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import './index.less'
 import { updateUserInfoIfNeeded } from 'store/actions/user'
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar'
+import Avatar from 'material-ui/Avatar'
 
 class TopBar extends Component {
   componentWillMount() {
@@ -12,19 +14,22 @@ class TopBar extends Component {
   render() {
     let { name, avatar, login } = this.props.user
 
-    return (<div className='component-top-bar'>
-      <div className='container'>
-        <div className='navi'>
-          <NavLink to='/' exact className='tab'>Home</NavLink>
+    return (
+      <div className='component-top-bar'>
+        <Toolbar className='container'>
+          <ToolbarGroup firstChild={ true } className='navi'>
+            <NavLink to='/' exact className='tab'>Home</NavLink>
+            <NavLink to='/test' exact className='tab'>Test</NavLink>
+          </ToolbarGroup>
 
-          <NavLink to='/test' exact className='tab'>Test</NavLink>
-        </div>
-
-        <Link to='/user' className='avatar'>
-          <img src={avatar}/> {name}
-        </Link>
+          <ToolbarGroup lastChild={ true }>
+            <Link to='/user' className='avatar-container'>
+              <Avatar className='avatar' src={ avatar }></Avatar>{ name }
+            </Link>
+          </ToolbarGroup>
+        </Toolbar>
       </div>
-    </div>)
+    )
   }
 }
 
